45d03d4d3f9a0b02d918667662b4ce644b2c9514
Dmitriy <thebrootos@gmail.com>
2021-08-12 09:35:55 +0500

    for review 12.08.2021

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 
 from . import views
 
+app_name = 'posts'
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('group/<slug:slug>/', views.group_posts, name='group_list'),
