1a93cded96604798c4ac2de1eb4857eb359fa1f8
Dmitriy <thebrootos@gmail.com>
2021-08-12 12:54:48 +0500

    11.08.2021 for review. Final!

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -7,20 +7,32 @@ User = get_user_model()
 
 
 class Group(models.Model):
-    title = models.CharField(max_length=200)
-    slug = models.SlugField(max_length=200, unique=True)
-    description = models.TextField(max_length=400)
+    title = models.CharField(max_length=200, verbose_name='Заголовок')
+    slug = models.SlugField(max_length=200, unique=True, verbose_name='ЧПУ')
+    description = models.TextField(max_length=400, verbose_name='Описание')
 
     def __str__(self):
         return self.title
 
+    class Meta:
+        verbose_name_plural = 'Группы'
+        verbose_name = 'Группа'
+
 
 class Post(models.Model):
-    text = models.TextField(max_length=400)
-    pub_date = models.DateTimeField(auto_now_add=True)
+    text = models.TextField(
+        max_length=400,
+        verbose_name='Текст'
+    )
+    pub_date = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Дата публикации'
+    )
     author = models.ForeignKey(
         User,
-        on_delete=models.CASCADE
+        on_delete=models.CASCADE,
+        related_name='posts',
+        verbose_name='Автор'
     )
     group = models.ForeignKey(
         Group,
@@ -32,3 +44,5 @@ class Post(models.Model):
 
     class Meta:
         ordering = ['-pub_date']
+        verbose_name_plural = 'Посты'
+        verbose_name = 'Пост'
