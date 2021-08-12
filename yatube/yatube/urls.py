e6626df1796c14bec5d717d42981afe23e1f15c2
Dmitriy <thebrootos@gmail.com>
2021-08-12 09:55:57 +0500

    for review 12.08.2021 - fix namespaces

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('', include('posts.urls', namespace='posts')),
+    path('', include('posts.urls', namespace='index')),
     path('group/<slug:slug>/', include('posts.urls', namespace='posts')),
     path('admin/', admin.site.urls),
 ]
