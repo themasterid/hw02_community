08e67a05ab758cd4c5bb63bc91d3bab39c8066c7
Dmitriy <thebrootos@gmail.com>
2021-08-24 11:06:26 +0500

    fix slug 24.08.2021

--- yatube/users/views.py
+++ yatube/users/views.py
@@ -7,5 +7,5 @@ from .forms import CreationForm
 
 class SignUp(CreateView):
     form_class = CreationForm
-    success_url = reverse_lazy('posts:index')  # ! FIX?
+    success_url = reverse_lazy('posts:index')
     template_name = 'users/signup.html'
