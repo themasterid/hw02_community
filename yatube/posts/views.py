5448020dfad5cd0d036c378989989de77819aafa
themasterid <thebrootos@gmail.com>
2022-07-11 00:15:22 +0500

    fix 11.7.2022

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -4,15 +4,19 @@ from .models import Group, Post
 
 
 def index(request):
-    posts = Post.objects.all()[:10]
+    """Выводим на страницу первые 10 записей постов."""
+
+    posts = Post.objects.select_related('author')[:10]
     template = 'posts/index.html'
     context = {'posts': posts}
     return render(request, template, context)
 
 
 def group_posts(request, slug):
+    """Выводим на страницу первые 10 записей групп."""
+
     group = get_object_or_404(Group, slug=slug)
-    posts = group.posts.all()[:10]
+    posts = group.posts.select_related('author')[:10]
     template = 'posts/group_list.html'
     context = {
         'group': group,
