79d1f33d2b69c1fc3cca204417066a04c77e6a2b
themasterid <thebrootos@gmail.com>
2022-07-10 22:44:35 +0500

    fix 3 sprint

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -17,5 +17,16 @@ class PostAdmin(admin.ModelAdmin):
     empty_value_display = '-пусто-'
 
 
+class GroupAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'slug',
+        'description',
+    )
+    search_fields = ('slug',)
+    list_filter = ('title',)
+    empty_value_display = '-пусто-'
+
+
 admin.site.register(Post, PostAdmin)
-admin.site.register(Group)
+admin.site.register(Group, GroupAdmin)
